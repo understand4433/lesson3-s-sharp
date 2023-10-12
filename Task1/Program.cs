49dc0c85dc18a89ff3f18c79e776eefcd52efcb9
yanepumba <adress>
2023-10-12 20:40:45 +0300

    add Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿// 1. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int enterCoordinate(string message)
+{ // Ввод переменных.
+    System.Console.WriteLine(message); // Получаем данные из строки.
+    string value = Console.ReadLine(); // Вызов консольного ввода, value - значение, которое вводим.
+    return int.Parse(value); // возрвращаем переменную в int.
+}
+
+bool checkValidate(int x, int y)
+{ // проверку ввода пользователя что x и y не равен 0.
+    if (x == 0 || y == 0)
+    { // если x = 0 и y = 0, || - или.
+        Console.WriteLine("Координата X или Y равна нулю!"); // 
+        return false; // проверка не прошла.
+    }
+    return true; // проверка прошла.
+}
+
+int checkCategory(int x, int y)
+{ // за определение категории, где находится координата.
+    if (x > 0 && y > 0)
+    { // определяем номер четверти.
+        return 1;
+    }
+
+    if (x < 0 && y > 0)
+    { // определяем номер четверти.
+        return 2;
+    }
+
+    if (x < 0 && y < 0)
+    { // определяем номер четверти.
+        return 3;
+    }
+
+    return 4; // остаток, если всё false. 
+}
+
+int x = enterCoordinate("Введите координату X: "); // вызов функции enterCoordinate
+int y = enterCoordinate("Введите координату Y: ");
+
+if (checkValidate(x, y))
+{ // исполняемый код.
+    System.Console.WriteLine("Номер четверти равен: " + checkCategory(x, y));
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача2:
+//  Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+int readQuater(string message)
+{
+    System.Console.WriteLine(message); // Получаем данные из строки.
+    string value = Console.ReadLine();// Вызов консольного ввода, value - значение, которое вводим.
+    return int.Parse(value); // Возрвращаем переменную в int.
+}
+int quater = readQuater("Введите номер четверти:");
+if (quater > 0 && quater <= 5)
+{
+    if (quater == 1)
+    {
+        System.Console.WriteLine("X>0 и Y>0");
+    }
+    if (quater == 2)
+    {
+        System.Console.WriteLine("X<0 и Y>0");
+    }
+    if (quater == 3)
+    {
+        System.Console.WriteLine("X<0 и Y<0");
+    }
+    if (quater == 4)
+    {
+        System.Console.WriteLine("X>0 и Y<0");
+    }
+}
+else System.Console.WriteLine("Данные введены неверно.");
