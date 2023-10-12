cd44448191ac2392a810f9aa282782fb89508a9b
yanepumba <adress>
2023-10-12 20:55:16 +0300

    update Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,5 @@
-﻿// Задача2:
-//  Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+﻿//Задача 2.
+//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 int readQuater(string message)
 {
     System.Console.WriteLine(message); // Получаем данные из строки.
@@ -7,23 +7,30 @@ int readQuater(string message)
     return int.Parse(value); // Возрвращаем переменную в int.
 }
 int quater = readQuater("Введите номер четверти:");
-if (quater > 0 && quater <= 5)
+if (quater > 0 && quater < 5)
 {
-    if (quater == 1)
+    switch (quater)
     {
-        System.Console.WriteLine("X>0 и Y>0");
+        case 1: System.Console.WriteLine("X>0 и Y>0"); break;
+        case 2: System.Console.WriteLine("X<0 и Y>0"); break;
+        case 3: System.Console.WriteLine("X<0 и Y<0"); break;
+        default: System.Console.WriteLine("X>0 и Y<0"); break;
     }
-    if (quater == 2)
-    {
-        System.Console.WriteLine("X<0 и Y>0");
-    }
-    if (quater == 3)
-    {
-        System.Console.WriteLine("X<0 и Y<0");
-    }
-    if (quater == 4)
-    {
-        System.Console.WriteLine("X>0 и Y<0");
-    }
+    // if (quater == 1)
+    // {
+    //     System.Console.WriteLine("X>0 и Y>0");
+    // }
+    // if (quater == 2)
+    // {
+    //     System.Console.WriteLine("X<0 и Y>0");
+    // }
+    // if (quater == 3)
+    // {
+    //     System.Console.WriteLine("X<0 и Y<0");
+    // }
+    // if (quater == 4)
+    // {
+    //     System.Console.WriteLine("X>0 и Y<0");
+    // }
 }
 else System.Console.WriteLine("Данные введены неверно.");
